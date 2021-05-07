7af11323d03525ff78a2fdbe76c7e9e00c2a748a
Helio Vicente Viegas <heliovicenteviegas@gmail.com>
2021-05-07 13:02:07 +0100

    Guardar lista itens

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,26 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+/*
+this.listGiff=
+       [
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/l0Iy797ZhIvLhm0Io/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/X8mtsz8N5CBGv4WSgr/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/GCvktC0KFy9l6/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/yP6rlwwW7Rdu0/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+        {url:'https://media.giphy.com/media/m2WQBH7DhSdgLfnTUi/giphy.gif'},
+
+        
+        ]
+
+
+*/
